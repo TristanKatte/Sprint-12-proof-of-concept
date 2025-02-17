51d76a058e249a5e7a3db807e8dbecf38bb338f6
TristanKatte <tristankatte@hotmail.com>
2025-02-17 14:41:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -42,17 +42,78 @@ app.get('/linkedin', function(request, response) {
 });
 
 app.get('/google', async function(request, response) {
-  const [apiResponse] = await analyticsDataClient.runReport({
-    property: `properties/${propertyId}`,
-    dateRanges: [{ startDate: '2024-03-31', endDate: 'today' }],
-    dimensions: [{ name: 'country' }],
-    metrics: [{ name: 'activeUsers' }],
-  });
-  
-  // Structure the data with rows property
-  const googleAd = {
-    rows: apiResponse.rows || []
-  };
-  
-  response.render('google', { googleAd });
-});
+	let propertyId = '301922918';
+
+	// Using a default constructor instructs the client to use the credentials
+	// specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
+	const analyticsDataClient = new BetaAnalyticsDataClient();
+
+	// Runs a simple report.
+	const [res] = await analyticsDataClient.runReport({
+		property: `properties/${propertyId}`,
+		dateRanges: [
+			{
+				// De startdatum is de oprichtdatum van Fresk, dit zijn dus all-time gegevens!
+				startDate: '2022-03-09',
+				endDate: 'today',
+			},
+			],
+			dimensions: [
+			{
+				name: 'country',
+			},
+			],
+			metrics: [
+			{
+				name: 'activeUsers',
+			},
+		],
+	});
+	
+	const [apiCity] = await analyticsDataClient.runReport({
+		property: `properties/${propertyId}`,
+		dateRanges: [
+			{ 
+				// De startdatum is de oprichtdatum van Fresk, dit zijn dus all-time gegevens!
+				startDate: '2024-01-01',
+				endDate: 'today', 
+			},
+			],
+			dimensions: [
+			{ 
+				name: 'city', 
+			},
+			],
+			metrics: [
+			{   
+				name: 'activeUsers', 
+			},
+		],
+	  });
+
+	const [apiBrowser] = await analyticsDataClient.runReport({
+		property: `properties/${propertyId}`,
+		dateRanges: [
+			{ 
+				startDate: '2023-06-01',
+				endDate: 'today', 
+			},
+			],
+			dimensions: [
+			{ 
+				name: 'browser',
+			},
+			],
+			metrics: [
+			{   
+				name: 'activeUsers', 
+			},
+		],
+	  });
+
+	response.render('google', {
+    	city : apiCity,
+		browser: apiBrowser,
+		rows: res.rows
+	})
+})
